d0f507c588f2fb0afae8ae9cd21936522783f08c
ginandiniputri <ginandiniputri@gmail.com>
2017-04-01 14:15:59 +0700

    Add files via upload

---
+++ assignment/assignment_1/1301140250/equi.py
@@ -0,0 +1,28 @@
+"""def equi(a):
+    sum_left, sum_right = 0, sum(a)
+    for index,value in enumerate(a):
+        sum_right -= value
+        if sum_left == sum_right:
+            yield index
+        sum_left += value
+
+list(solution([-7, 1, 5, 2, -4, 3, 0]))"""
+
+
+n = input("Input : ")
+k = []
+for x in xrange(1, n+1):
+	k.append(input("Bilangan ke " + str(x) + ":"))
+
+sum_left = 0
+sum_right = sum (k[1:])
+
+arr = []
+
+for i in range(n):
+		if (i != 0):
+			sum_left = sum_left + k[i-1]
+			sum_right = sum_right - k[i]
+		if (sum_left == sum_right):
+			arr.append(i)
+print(arr)
